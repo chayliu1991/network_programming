377572d5dd468e5fde4bdf368dd9cd73cca3cfe0
liuchuanyang <liuchuanyang@seetrum.com>
2021-08-21 18:21:22 +0800

    add reliable tcp

---
+++ codes/reliable_client.c
@@ -0,0 +1,65 @@
+#include <errno.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#define MESSAGE_SIZE 102400000
+#define SERV_PORT 12345
+
+int tcp_client(char *address, int port) {
+  int socket_fd;
+  socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+
+  struct sockaddr_in server_addr;
+  bzero(&server_addr, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(port);
+  inet_pton(AF_INET, address, &server_addr.sin_addr);
+
+  socklen_t server_len = sizeof(server_addr);
+  int connect_rt =
+      connect(socket_fd, (struct sockaddr *)&server_addr, server_len);
+  if (connect_rt < 0) {
+    fprintf(stderr, "connect() failed:%s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  return socket_fd;
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    printf("usage:%s <Ipaddress>\n", argv[0]);
+    exit(EXIT_SUCCESS);
+  }
+
+  int socket_fd = tcp_client(argv[1], SERV_PORT);
+  char buf[129];
+  int len;
+  int rc;
+
+  while (fgets(buf, sizeof(buf), stdin) != NULL) {
+    len = strlen(buf);
+    rc = send(socket_fd, buf, len, 0);
+    if (rc < 0) {
+      fprintf(stderr, "send() failed:%s\n", strerror(errno));
+      exit(EXIT_FAILURE);
+    }
+    sleep(3);
+    rc = read(socket_fd, buf, sizeof(buf));
+    if (rc < 0) {
+      fprintf(stderr, "read() failed:%s\n", strerror(errno));
+      exit(EXIT_FAILURE);
+    } else if (rc == 0) {
+      fprintf(stderr, "peer connection closed\n");
+      exit(EXIT_FAILURE);
+    } else
+      fputs(buf, stdout);
+  }
+  exit(0);
+}
---
+++ codes/reliable_server.c
@@ -0,0 +1,82 @@
+#include <errno.h>
+#include <netinet/in.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <unistd.h>
+
+#define SERV_PORT 12345
+
+int tcp_server(int port) {
+  int listenfd;
+  listenfd = socket(AF_INET, SOCK_STREAM, 0);
+
+  struct sockaddr_in server_addr;
+  bzero(&server_addr, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+  server_addr.sin_port = htons(port);
+
+  int on = 1;
+  setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on));
+
+  int rt1 =
+      bind(listenfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (rt1 < 0) {
+    fprintf(stderr, "bind() failed:%s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  int rt2 = listen(listenfd, 1024);
+  if (rt2 < 0) {
+    fprintf(stderr, "listen() failed:%s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  signal(SIGPIPE, SIG_IGN);
+
+  int connfd;
+  struct sockaddr_in client_addr;
+  socklen_t client_len = sizeof(client_addr);
+
+  if ((connfd = accept(listenfd, (struct sockaddr *)&client_addr,
+                       &client_len)) < 0) {
+    fprintf(stderr, "accept() failed:%s\n", strerror(errno));
+    exit(EXIT_FAILURE);
+  }
+
+  return connfd;
+}
+
+int main(int argc, char **argv) {
+  int connfd;
+  char buf[1024];
+
+  connfd = tcp_server(SERV_PORT);
+
+  for (;;) {
+    int n = read(connfd, buf, 1024);
+    if (n < 0) {
+      fprintf(stderr, "read() failed:%s\n", strerror(errno));
+      exit(EXIT_FAILURE);
+    } else if (n == 0) {
+      fprintf(stderr, "client closed\n");
+      exit(EXIT_SUCCESS);
+    }
+
+    sleep(5);
+
+    int write_nc = send(connfd, buf, n, 0);
+    printf("send bytes: %zu \n", write_nc);
+    if (write_nc < 0) {
+      fprintf(stderr, "send() failed:%s\n", strerror(errno));
+      exit(EXIT_FAILURE);
+    }
+  }
+
+  exit(0);
+}
