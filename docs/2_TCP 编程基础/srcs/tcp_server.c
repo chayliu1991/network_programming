4cc6c00ef7254b5fe840a2c7d3ac0aa61588f560
chay <chayliu@126.com>
2021-08-26 21:04:15 +0800

    update chapter 2

--- docs/2_TCP 编程基础/srcs/tcp_server.c
+++ docs/2_TCP 编程基础/srcs/tcp_server.c
@@ -18,31 +18,6 @@ void errExit(const char *caller)
     exit(EXIT_FAILURE);
 }
 
-ssize_t readn(int fd, void *vptr, size_t n)
-{
-    size_t nleft, nread;
-    char *ptr;
-
-    ptr = vptr;
-    nleft = n;
-
-    while (nleft > 0)
-    {
-        if ((nread = read(fd, ptr, nleft)) < 0)
-        {
-            if (errno == EINTR)
-                nread = 0; //@ call read again
-            else
-                return -1;
-        }
-        else if (nread == 0) //@ EOF
-            return 0;
-
-        nleft -= nread;
-        ptr += nread;
-    }
-    return (n - nleft);
-}
 
 ssize_t writen(int fd, void *vptr, size_t n)
 {
@@ -75,9 +50,53 @@ void sigchld_handler(int sig)
     return;
 }
 
+int echo(int fd)
+{
+    char buf[BUF_SIZE];
+    ssize_t n = 0;
+
+again:
+    while((n = read(fd,buf,BUF_SIZE-1)) >0)
+    {
+        buf[n] = '\0';
+        struct sockaddr_in client_addr;
+        socklen_t client_len = sizeof(client_addr);
+        if (getpeername(fd, (struct sockaddr *)&client_addr, &client_len) == -1)
+        {
+            printf("getpeername() error:%s\n", strerror(errno));
+        }
+        char ip[INET_ADDRSTRLEN] = {0};
+        inet_ntop(AF_INET, &client_addr.sin_addr, ip, INET_ADDRSTRLEN);
+        char info[64] = {0};
+        snprintf(info, sizeof(info), "%s:%u", ip, ntohs(client_addr.sin_port));
+
+        printf("Received From %s : %s\n", info, buf);
+        if (writen(fd, buf, n) != n)
+        {
+            printf("write error occured\n");
+            close(fd);
+            return 0;
+        }
+    }
+
+    if(n < 0 && errno == EINTR)
+         goto again;
+    
+    if(n == 0)
+    {
+        printf("peer client closed\n");
+        close(fd);   
+        return 0;  
+    }
+
+    printf("read error occured\n");
+    close(fd);
+    return -1;
+}
+
+
 int main(int argc, char *argv[])
 {
-
     struct sigaction act;
     sigemptyset(&act.sa_mask);
     act.sa_handler = sigchld_handler;
@@ -100,13 +119,11 @@ int main(int argc, char *argv[])
     if (listen(listen_fd, 1024) < 0)
         errExit("listen()");
 
-    char buf[BUF_SIZE];
     socklen_t client_len = sizeof(client_addr);
     int conn_fd;
     for (;;)
     {
         conn_fd = accept(listen_fd, (struct sockaddr *)&client_addr, &client_len);
-        printf("accetpt fd:%d\n", conn_fd);
         switch (fork())
         {
         case -1:
@@ -114,46 +131,7 @@ int main(int argc, char *argv[])
         case 0:
         {
             close(listen_fd); //@ 子进程中关闭监听套接字
-            printf("create child\n");
-            while (1)
-            {
-                bzero(buf, sizeof(buf));
-                ssize_t n = readn(conn_fd, buf, BUF_SIZE - 1);
-                printf("trying to read\n");
-
-                buf[n] = '\0';
-                if (n < -1)
-                {
-                    printf("read error occured\n");
-                    close(conn_fd);
-                    break;
-                }
-                else if (n == 0)
-                {
-                    printf("peer client closed\n");
-                    close(conn_fd);
-                    break;
-                }
-
-                struct sockaddr_in client_addr;
-                socklen_t client_len = sizeof(client_addr);
-                if (getpeername(conn_fd, (struct sockaddr *)&client_addr, &client_len) == -1)
-                {
-                    printf("getpeername() error:%s\n", strerror(errno));
-                }
-                char ip[INET_ADDRSTRLEN] = {0};
-                inet_ntop(AF_INET, &client_addr.sin_addr, ip, INET_ADDRSTRLEN);
-                char info[64] = {0};
-                snprintf(info, sizeof(info), "%s:%u", ip, ntohs(client_addr.sin_port));
-
-                printf("Received From %s : %s\n", info, buf);
-                if (writen(conn_fd, buf, n) != n)
-                {
-                    printf("write error occured\n");
-                    close(conn_fd);
-                    break;
-                }
-            }
+            echo(conn_fd);
             _exit(EXIT_SUCCESS);
         }
         default: //@ 父进程关闭连接套接字
